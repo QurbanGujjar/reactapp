964b76283fbe3fa9b081df297bdae5b8cf905551
Engineer Qurban Ali <42018848+QurbanGujjar@users.noreply.github.com>
2020-11-26 09:08:20 +0500

    update app.js with full name

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React With Qurban uuuuuaaahhhaa
+          Learn React With Qurban Ali Gujjar
         </a>
       </header>
     </div>
